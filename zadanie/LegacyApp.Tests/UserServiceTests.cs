e7a30e5637692a1811f5630e23efd8bf6bf370a1
Aleksandr Maslov <s24439@pjwstk.edu.pl>
2024-04-05 20:42:40 +0200

    Added "AddUser_ReturnsFalseWhenMissingAtSignAndDotInEmail" test

--- zadanie/LegacyApp.Tests/UserServiceTests.cs
+++ zadanie/LegacyApp.Tests/UserServiceTests.cs
@@ -40,4 +40,29 @@ public class UserServiceTests //RefactoringExample -> rmb -> add new solution ->
         // Assert
         Assert.Throws<ArgumentException>(action);
     }
+
+    [Fact]
+    public void AddUser_ReturnsFalseWhenMissingAtSignAndDotInEmail()
+    {
+        var userService = new UserService();
+
+        var result = userService.AddUser(
+            null,
+            "Kowalski",
+            "kowalskigmailcom",
+            DateTime.Parse("2000-01-01"),
+            1
+        );
+        
+        Assert.False(result);
+    }
+    
+    // AddUser_ReturnsFalseWhenMissingAtSignAndDotInEmail
+    // AddUser_ReturnsFalseWhenYoungerThen21YearsOld
+    // AddUser_ReturnsTrueWhenVeryImportantClient
+    // AddUser_ReturnsTrueWhenImportantClient
+    // AddUser_ReturnsTrueWhenNormalClient
+    // AddUser_ReturnsFalseWhenNormalClientWithNoCreditLimit
+    // AddUser_ThrowsExceptionWhenUserDoesNotExist
+    // AddUser_ThrowsExceptionWhenUserNoCreditLimitExistsForUser
 }
